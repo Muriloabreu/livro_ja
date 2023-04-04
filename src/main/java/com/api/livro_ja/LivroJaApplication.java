c1ff5208e3cd0f74a25a7f1fbd74875f91cc5b5a
muriloabreu <murilo.lilo@hotmail.com>
2023-04-03 22:42:19 -0300

    Inicialização da api Spring Boot - Livro_ja

---
+++ src/main/java/com/api/livro_ja/LivroJaApplication.java
@@ -0,0 +1,13 @@
+package com.api.livro_ja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/livro_ja/LivroJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.livro_ja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
