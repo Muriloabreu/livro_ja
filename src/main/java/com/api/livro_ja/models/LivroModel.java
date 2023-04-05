725115b1aef5195afffdb68b4086a054aab58c7f
muriloabreu <murilo.lilo@hotmail.com>
2023-04-04 23:54:49 -0300

    Criação classe LivroModel

--- src/main/java/com/api/livro_ja/models/LivroModel.java
+++ src/main/java/com/api/livro_ja/models/LivroModel.java
@@ -1,8 +1,13 @@
 package com.api.livro_ja.models;
 
 import java.io.Serializable;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,6 +15,68 @@ import jakarta.persistence.Table;
 public class LivroModel implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	@Column(nullable = false, unique = true, length = 50)
+	private String titulo;
+	@Column(nullable = false, unique = true, length = 30)
+	private String autor;
+	@Column(nullable = false, unique = true, length = 30)
+	private String lingua;
+	@Column(nullable = false, unique = true, length = 30)
+	private String editora;
+	@Column(nullable = false, unique = true, length = 10)
+	private int paginas;
+	@Column(nullable = false, unique = true, length = 30)
+	private String genero;
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getLingua() {
+		return lingua;
+	}
+	public void setLingua(String lingua) {
+		this.lingua = lingua;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	
 	
 	
