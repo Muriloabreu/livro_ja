9adb6b2bb3fc5d087fc220029e58b69f8714d7a5
muriloabreu <murilo.lilo@hotmail.com>
2023-04-04 23:57:50 -0300

    Criação Interface LivroRepository no Package Repositories

---
+++ src/main/java/com/api/livro_ja/repositories/LivroRepository.java
@@ -0,0 +1,13 @@
+package com.api.livro_ja.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.livro_ja.models.LivroModel;
+
+@Repository
+public interface LivroRepository extends JpaRepository<LivroModel, UUID>{
+
+}
