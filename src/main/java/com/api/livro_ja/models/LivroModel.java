34a9a1435ce50a702582772c82e4d9f862323a6b
muriloabreu <murilo.lilo@hotmail.com>
2023-04-04 23:42:43 -0300

    Criação do package Models

---
+++ src/main/java/com/api/livro_ja/models/LivroModel.java
@@ -0,0 +1,17 @@
+package com.api.livro_ja.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_LIVRO")
+public class LivroModel implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+
+}
