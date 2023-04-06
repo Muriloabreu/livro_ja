c2eecd01577ee43cd2af13dc0fd3d4bd7bc9797f
muriloabreu <murilo.lilo@hotmail.com>
2023-04-05 23:12:55 -0300

    Criação de classe Service Implements

---
+++ src/main/java/com/api/livro_ja/services/LivroServiceImpl.java
@@ -0,0 +1,46 @@
+package com.api.livro_ja.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.livro_ja.models.LivroModel;
+import com.api.livro_ja.repositories.LivroRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class LivroServiceImpl implements LivroService{
+
+	@Autowired
+	LivroRepository livroRepository;
+	
+	@Override
+	public List<LivroModel> findAll() {
+		
+		return livroRepository.findAll();
+	}
+
+	@Override
+	public LivroModel findById(UUID id) {
+		
+		return livroRepository.findById(id).get();
+	}
+
+	@Override
+	@Transactional
+	public LivroModel save(LivroModel livro) {
+		
+		return livroRepository.save(livro);
+	}
+
+	@Override
+	public void deleteLivro(LivroModel livro) {
+		
+		livroRepository.delete(null);
+		
+	}
+
+}
