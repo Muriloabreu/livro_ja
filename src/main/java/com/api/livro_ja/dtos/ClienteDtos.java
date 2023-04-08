1fd7c07a332111a0c64c338f47dc13bfa38309f3
muriloabreu <murilo.lilo@hotmail.com>
2023-04-08 15:11:15 -0300

    Criação da classe ClienteDtos

---
+++ src/main/java/com/api/livro_ja/dtos/ClienteDtos.java
@@ -0,0 +1,43 @@
+package com.api.livro_ja.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+public class ClienteDtos {
+	
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String sobreNome;
+	@NotBlank
+	private String cpf;
+	@NotBlank
+	private String telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
+}
