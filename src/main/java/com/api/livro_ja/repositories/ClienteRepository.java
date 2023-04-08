9c4e578bfa089da7d6d3d22a092526321ff32af7
muriloabreu <murilo.lilo@hotmail.com>
2023-04-08 15:08:26 -0300

    Criação da interface ClienteRepository

---
+++ src/main/java/com/api/livro_ja/repositories/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.api.livro_ja.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.livro_ja.models.ClienteModel;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModel, UUID>{
+
+}
