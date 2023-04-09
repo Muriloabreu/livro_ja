fee08ed3759b5d31622531ab2dd746e31823dc09
muriloabreu <murilo.lilo@hotmail.com>
2023-04-09 09:32:53 -0300

    Criação Classe AluguelServiceImpl

---
+++ src/main/java/com/api/livro_ja/services/AluguelServiceImpl.java
@@ -0,0 +1,48 @@
+package com.api.livro_ja.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.livro_ja.models.AluguelModel;
+import com.api.livro_ja.repositories.AluguelRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class AluguelServiceImpl implements AluguelService{
+	
+	@Autowired
+	AluguelRepository aluguelRepository;
+
+	@Override
+	public List<AluguelModel> findAll() {
+		
+		return aluguelRepository.findAll();
+	}
+
+	@Override
+	public Optional<AluguelModel> findById(UUID id) {
+		
+		return aluguelRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public AluguelModel save(AluguelModel aluguel) {
+		
+		return aluguelRepository.save(aluguel);
+	}
+
+	@Override
+	@Transactional
+	public void deleteAluguel(AluguelModel aluguel) {
+		
+		aluguelRepository.delete(aluguel);
+		
+	}
+
+}
