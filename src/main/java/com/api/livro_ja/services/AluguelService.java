6326fc70111144ae3c0e1857ffd19a1a1121a0ad
muriloabreu <murilo.lilo@hotmail.com>
2023-04-09 09:32:16 -0300

    Criação Interface AluguelService

---
+++ src/main/java/com/api/livro_ja/services/AluguelService.java
@@ -0,0 +1,18 @@
+package com.api.livro_ja.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.api.livro_ja.models.AluguelModel;
+
+
+public interface AluguelService {
+	
+	List<AluguelModel> findAll();
+	Optional<AluguelModel> findById(UUID id);
+	AluguelModel save(AluguelModel aluguel);
+	void deleteAluguel(AluguelModel aluguel);
+	
+
+}
