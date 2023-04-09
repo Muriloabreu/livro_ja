5b6ecd4e87776976727e357caa068e47d997b00e
muriloabreu <murilo.lilo@hotmail.com>
2023-04-09 09:31:33 -0300

    Criação Interface AluguelRepository

---
+++ src/main/java/com/api/livro_ja/repositories/AluguelRepository.java
@@ -0,0 +1,11 @@
+package com.api.livro_ja.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.livro_ja.models.AluguelModel;
+
+public interface AluguelRepository extends JpaRepository<AluguelModel, UUID> {
+
+}
