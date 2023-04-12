b0638ad298944426fb1ffd1f76de059c54c210dd
muriloabreu <murilo.lilo@hotmail.com>
2023-04-11 21:14:59 -0300

    Implementação classe LivroModel removendo atributo

--- src/main/java/com/api/livro_ja/models/LivroModel.java
+++ src/main/java/com/api/livro_ja/models/LivroModel.java
@@ -1,8 +1,7 @@
 package com.api.livro_ja.models;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+
+
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -10,15 +9,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+
 
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TB_LIVRO")
-public class LivroModel implements Serializable {
+public class LivroModel  {
 	
-	private static final long serialVersionUID = 1L;
+	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -36,8 +35,6 @@ public class LivroModel implements Serializable {
 	@Column(nullable = false,  length = 30)
 	private String genero;
 	
-	@OneToMany(mappedBy = "livro")
-	private List<AluguelModel> alugueis = new ArrayList<>();
 	
 	
 	public UUID getId() {
@@ -81,9 +78,6 @@ public class LivroModel implements Serializable {
 	}
 	public void setGenero(String genero) {
 		this.genero = genero;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 	
 	
